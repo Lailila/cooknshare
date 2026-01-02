efaca623b37c4608fb46b144b83ed3b8f1995ee9
大西陽南子 <hinakoonishi@HinaWollMB.local>
2026-01-02 13:35:59 +0100

    Login erstellt

--- pages/profil.php
+++ pages/profil.php
@@ -1,13 +1,76 @@
+<<<<<<< HEAD
 <?php 
   $title = 'Mein Profil';
   include "../includes/header.php";
 ?>
 
 <?php
+=======
+<?php
+require "../includes/secure.php"; 
+$currentPage = 'profil';
+require "../includes/db.php";
+>>>>>>> 246e07cf3e89f6ba7984c436d97b4df0f349833f
 //$userId = $_SESSION['user_id']; //sobald login aktuell ist
 
 //test-user:
-$userId = 7;
+//$userId = 7;
+if(isset($_POST['upload_image']) && isset($_FILES['profile_image'])){
+  $file = $_FILES['profile_image'];
+  $errors = [];
+
+  if($file['error'] !== 0){
+    $errors[] = "Datei konnte nicht hochgeladen werden.";
+  }
+
+  if(!empty($file['tmp_name']) && !getimagesize($file['tmp_name'])){
+    $errors[] = "Die Datei ist kein gültiges Bild.";
+  }
+
+  $allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+  if(!in_array($file['type'], $allowedTypes)){
+    $errors[] = "Nur JPG, PNG oder WEBP erlaubt.";
+  }
+
+  if(!empty($errors)){
+    $_SESSION['upload_errors'] = $errors;
+    header("Location: profil.php");
+    exit;
+  }
+
+
+  //ohne Fehler geklappt:
+  $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+  $newFileName = 'user_' . $userId . '_' . time() . '.' . $extension;
+  $uploadDir = __DIR__ . "/../uploads/profile/";
+  $uploadPath = $uploadDir . $newFileName;
+
+  if(!is_dir($uploadDir)){
+    mkdir($uploadDir, 0755, true);
+  }
+
+  if(!is_uploaded_file($file['tmp_name'])){
+    die("Keine echte Upload Datei");
+  }
+
+  if(!move_uploaded_file($file['tmp_name'], $uploadPath)) {
+    $_SESSION['upload_errors'] = ["Die Datei konnte nicht gespeichert werden."];
+    header("Location: profil.php");
+    exit;
+  }
+
+  $statementPicture = $conn->prepare("UPDATE users SET image_path = ? WHERE id = ?");
+  $statementPicture->bind_param("si", $newFileName, $userId);
+  $statementPicture->execute();
+  $statementPicture->close();
+
+  $_SESSION['upload_success'] = "Profilbild erfolgreich aktualisiert.";
+  header("Location: profil.php");
+  exit;
+
+
+}
+
 $statement = $conn->prepare("
 SELECT username, email, created_at, image_path FROM users WHERE id = ?
 ");
@@ -36,8 +99,25 @@ require "../includes/header.php"; ?>
   <div class="container-fluid">
     <div class="row">
     <div class="col-lg-4 col-12 text-center">
-      <img src="<?= $user['image_path'] ? '../uploads/' . htmlspecialchars($user['image_path']) : '../img/profile-default.svg' ?>" alt="" class="img-fluid rounded-5 mb-5">
-      <button class="btn btn-primary fs-4">neues Profilbild hochladen</button>
+      <?php if (!empty($_SESSION['upload_errors'])): ?>
+        <div class="alert alert-danger fs-5">
+          <ul class="mb-0">
+            <?php foreach ($_SESSION['upload_errors'] as $error): ?>
+            <li><?=  htmlspecialchars($error) ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php unset($_SESSION['upload_errors']); endif; ?>
+
+      <?php if(!empty($_SESSION['upload_success'])): ?>
+        <div class="alert alert-success fs-5"><?= htmlspecialchars($_SESSION['upload_success']) ?></div>
+      <?php unset($_SESSION['upload_success']); endif; ?>
+
+      <img src="<?= $user['image_path'] ? '../uploads/profile/' . htmlspecialchars($user['image_path']) : '../img/profile-default.svg' ?>" alt="" class="img-fluid rounded-5 mb-5">
+      <form action="profil.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="profile_image" class="form-control mb-3" accept="image/*" required>
+        <button type="submit" name="upload_image" class="btn btn-primary fs-4">neues Profilbild hochladen</button>
+      </form>
     </div>
     <div class="col-lg-8 col-12">
       <table class="table table-hover fs-4">
