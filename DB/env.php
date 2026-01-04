1b5119abd4b2b9d5cb3cde0e2a777980547893a5
Lailila <sophia-rath@live.de>
2026-01-04 17:56:03 +0100

    Remove DB/env.php from repository and ignore it

--- DB/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST','localhost');
-define('DB_NAME','cooknshare');
-//gib deinen eigenen Username vom DB in ''
-define('DB_USER','');
-//dein Passwort auch
-define('DB_PASS','');
